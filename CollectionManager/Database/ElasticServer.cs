4ff43ddfd7adbb8574411d090b677f74f89d490c
Antonio Schonmann <antonio_schonmann@hotmail.com>
2017-04-30 17:41:02 -0300

    Basic auth;

--- CollectionManager/Database/ElasticServer.cs
+++ CollectionManager/Database/ElasticServer.cs
@@ -5,24 +5,31 @@ using System.Configuration;
 
 namespace CollectionManager.Database
 {
-    //Singleton class to manage ElasticSearcg database connection.
+    //Singleton to prepare ElasticSearch database client.
     public static class ElasticServer
     {
         private static ElasticClient elastic;
+        private static String[] indexes = { Item.DEFAULT_INDEX, Place.DEFAULT_INDEX, Person.DEFAULT_INDEX };
+
         public static void Initialize()
         {
+            //Get connection parameters from app settings.
             var elasticAddress = ConfigurationManager.AppSettings.Get("ElasticAddress");
             var elasticUser = ConfigurationManager.AppSettings.Get("ElasticUser");
             var elasticPassword = ConfigurationManager.AppSettings.Get("ElasticPassword");
+            //Add settings to set index mapping for the model classes and basic auth parameters.
             var settings = new ConnectionSettings(new Uri(elasticAddress))
                 .MapDefaultTypeIndices(m => m
                 .Add(typeof(Item), Item.DEFAULT_INDEX)
                 .Add(typeof(Place), Place.DEFAULT_INDEX)
-                .Add(typeof(Person), Person.DEFAULT_INDEX));
+                .Add(typeof(Person), Person.DEFAULT_INDEX))
+                .BasicAuthentication(elasticUser, elasticPassword);
+            //Initialize elastic client with our predefined settings.
             elastic = new ElasticClient(settings);
-            elastic.CreateIndex(Item.DEFAULT_INDEX);
-            elastic.CreateIndex(Place.DEFAULT_INDEX);
-            elastic.CreateIndex(Person.DEFAULT_INDEX);
+            //Create needed indexes.
+            foreach(var idx in indexes){
+                elastic.CreateIndex(idx);
+            }
         }
         public static ElasticClient GetClient()
         {
